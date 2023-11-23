ab303970637c5fc15a088c5c7189abe80c941d1e
Aryaman Bhutani <aryaman276@gmail.com>
2023-11-23 17:20:37 +0530

    data run

--- param_generator.py
+++ param_generator.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 # initial values of the parameters
-N = 500
+N = np.logspace(1, 3.6, 30)
 v = 0.03
-L = 35.36
+L = 20
 dt = 1
-t_max = 1000
+t_max = 500
 R = 1
 eta = 3
 seed = 0
@@ -13,13 +13,13 @@ seed = 0
 n = 150
 params = np.zeros((n, 8))
 for i in range(n):
-    params[i, 0] = int(N)
+    params[i, 0] = int(N[i%30])
     params[i, 1] = v
     params[i, 2] = L
     params[i, 3] = dt
     params[i, 4] = t_max
     params[i, 5] = R
-    params[i, 6] = (0.2*i)%30
+    params[i, 6] = 3
     params[i, 7] = i//30
     
 np.savetxt('params.txt', params, delimiter=',')
