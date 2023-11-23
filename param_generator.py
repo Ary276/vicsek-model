197822e4d742b6a336162b85bfa4457125433491
Aryaman Bhutani <aryaman276@gmail.com>
2023-11-23 14:30:01 +0530

    data run for critical exponents

--- param_generator.py
+++ param_generator.py
@@ -1,26 +1,26 @@
 import numpy as np
 
 # initial values of the parameters
-N = np.logspace(1, 3.6, 50)
-v = 0.3
-L = [3.1, 5, 10, 31.6, 50]
+N = 400
+v = 0.03
+L = 31.6
 dt = 1
 t_max = 1000
 R = 1
 eta = 3
 seed = 0
 
-n = 50
+n = 150
 params = np.zeros((n, 8))
 for i in range(n):
-    params[i, 0] = int(N[i])
+    params[i, 0] = int(N)
     params[i, 1] = v
-    params[i, 2] = 20
+    params[i, 2] = L
     params[i, 3] = dt
     params[i, 4] = t_max
     params[i, 5] = R
-    params[i, 6] = 2.5
-    params[i, 7] = 0
+    params[i, 6] = (0.2*i)%30
+    params[i, 7] = i//30
     
 np.savetxt('params.txt', params, delimiter=',')
 
