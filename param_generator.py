336d7d99d611f530b7225ec7f0e550af09be2be2
Aryaman Bhutani <aryaman276@gmail.com>
2023-11-24 05:31:19 +0530

    data run

--- param_generator.py
+++ param_generator.py
@@ -12,7 +12,7 @@ eta = 2.4
 seed = 0
 fov = np.linspace(0, 2*np.pi, 50)
 
-n = 50
+n = 500
 params = np.zeros((n, 9))
 for i in range(n):
     params[i, 0] = N
@@ -23,7 +23,7 @@ for i in range(n):
     params[i, 5] = R
     params[i, 6] = 2
     params[i, 7] = 0
-    params[i, 8] = fov[i]
+    params[i, 8] = fov[i%50]
     
 np.savetxt('params.txt', params, delimiter=',')
 
